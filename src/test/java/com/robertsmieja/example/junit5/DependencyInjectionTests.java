e8fcc39cb6e88fc8e3bc31cc6cc13aed9cf63b00
Robert Smieja <robertsmieja@gmail.com>
2017-06-18 23:04:16 -0400

    Add DependencyInjectionTests

---
+++ src/test/java/com/robertsmieja/example/junit5/DependencyInjectionTests.java
@@ -0,0 +1,54 @@
+package com.robertsmieja.example.junit5;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("dependency injection")
+@ExtendWith(DependencyInjectionTests.SimpleIntegerExtension.class)
+public class DependencyInjectionTests {
+    Calculator objectUnderTest;
+
+    @BeforeEach
+    void setup(TestInfo testInfo){
+        printTestInfo(testInfo);
+        objectUnderTest = new Calculator();
+    }
+
+    @Test
+    @Tag("second tag")
+    //Multiple dependencies can be injected
+    void simpleAddition(TestInfo testInfo, TestReporter testReporter){
+        printTestInfo(testInfo);
+        assertEquals(2, objectUnderTest.add(1, 1));
+
+        //TestReporter is intended as a replacement to STDOUT/STDERR
+        testReporter.publishEntry("key", "value");
+    }
+
+    @Test
+    void simpleAdditionWithCustomDependencyInjected(TestInfo testInfo, Integer valueToAdd){
+        printTestInfo(testInfo);
+        assertEquals(4, objectUnderTest.add(valueToAdd, valueToAdd));
+    }
+
+    private void printTestInfo(TestInfo testInfo){
+        System.out.println("Display Name: <" + testInfo.getDisplayName() + ">");
+        System.out.println("Tags: <" + testInfo.getTags() + ">");
+        System.out.println("Test Class: <" + testInfo.getTestClass().get() + ">");
+        System.out.println("Test Method: <" + testInfo.getTestMethod().get() + ">");
+    }
+
+    public static class SimpleIntegerExtension implements ParameterResolver{
+        @Override
+        public boolean supports(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+            return parameterContext.getParameter().getType().equals(Integer.class);
+        }
+
+        @Override
+        public Object resolve(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+            return Integer.valueOf(2);
+        }
+    }
+}
