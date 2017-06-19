58e113b6154040dbdf24caaeaae5ce02951d6cac
Robert Smieja <robertsmieja@gmail.com>
2017-06-18 21:11:29 -0400

    Tweak TagTests.java

--- src/test/java/com/robertsmieja/example/junit5/TagTests.java
+++ src/test/java/com/robertsmieja/example/junit5/TagTests.java
@@ -3,13 +3,16 @@ package com.robertsmieja.example.junit5;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-@Tag("example")
+import static org.junit.Assert.assertEquals;
+
 @Tag("tag")
+@Tag("example")
 public class TagTests {
 
-    @Tag("fast")
     @Test
-    void testMethod(){
+    @Tag("fast")
+    void testAddition(){
         System.out.println("TagTests.testMethod() executed");
+        assertEquals(3, new Calculator().add(1, 3));
     }
 }
